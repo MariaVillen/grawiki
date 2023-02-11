fb4b92ff975afe2deb4069164d32dbee95fd819e
Maria Villen <9887543+MariaVillen@users.noreply.github.com>
2023-02-11 15:59:37 +0100

    stylesguide and assets added

---
+++ client/src/assets/index.jsx
@@ -0,0 +1,6 @@
+// Logos
+export { default as LogoStretch } from "./logo/Logo_slogan_v.svg";
+export { default as LogoHorizontal } from "./logo/Logo_slogan_H.svg";
+export { default as logoBackground } from "./logo/logo.svg";
+
+//Iconos
