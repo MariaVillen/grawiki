1e5cf6f0cdec265e278605106864ff5b233cb7ef
Maria Villen <9887543+MariaVillen@users.noreply.github.com>
2023-03-13 19:55:25 +0100

    adding mock services

--- client/src/pages/home.page.jsx
+++ client/src/pages/home.page.jsx
@@ -1,10 +1,37 @@
-import Searchbar from "../components/FormLibrary/searchbar/searchbar";
+import { Searchbar, ArticleCard, Spinner } from "../components";
 import classes from "./pages-styles/home.module.sass";
+import useServices from "../services/useMockServices";
 
 function Home() {
+  const [articles, setArticles] = useState([]);
+  const [isLoading, setIsLoading] = useState([true]);
+  const [getArticles] = useServices();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const data = getArticles(articles);
+        setArticles(data);
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+  }, []);
+
   return (
     <div className={classes.container}>
       <Searchbar />
+      <div className={classes.articles}>
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          articles &&
+          articles.lenght > 0 &&
+          articles.map((article) => (
+            <ArticleCard key={article.id} data={article} />
+          ))
+        )}
+      </div>
     </div>
   );
 }
